62b63ea54c05d502bc2586f3c7960708ee9a2be5
Jesus Bossa <x@jesusbossa.dev>
2022-03-10 15:13:41 -0500

    chore: imporve blogs articles styles

--- layout/Blogs/Blogs.styles.ts
+++ layout/Blogs/Blogs.styles.ts
@@ -8,4 +8,27 @@ export const BlogsSection = css({
     borderTopLeftRadius: '4px',
     borderTopRightRadius: '4px',
   },
+
+  '& article h6, & article p': {
+    overflowY: 'hidden',
+    display: '-webkit-box',
+    '-webkit-line-clamp': 3,
+    '-webkit-box-orient': 'vertical',
+    transition: 'all 0.3s ease 0s',
+  },
+
+  '& article h6:hover, & article p:hover': {
+    height: '70.5px',
+    display: 'flex',
+    overflowY: 'scroll',
+    boxShadow: '0px -6px 11px 0px rgba(0,0,0,0.08) inset',
+  },
+
+  '& article p:hover': {
+    height: '49.1px',
+  },
+
+  '& article .card-body': {
+    minHeight: '81.5px',
+  },
 });
