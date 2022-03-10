f1d28dd633bef5ef9b07f55f7638a9182aab6ddb
Jesus Bossa <x@jesusbossa.dev>
2022-03-09 20:50:55 -0500

    chore: improve info

--- layout/Info/Info.tsx
+++ layout/Info/Info.tsx
@@ -2,9 +2,33 @@ import { FluidContainer } from '@makinox/makinox-ui';
 
 export default function Info() {
   return (
-    <section className={FluidContainer()}>
-      <p>Section expilcando de que se trata la pagina</p>
-      <p>La idea es que sean dos parrafos</p>
+    <section className={`flex justify-evenly ${FluidContainer()}`}>
+      <article>
+        <p>Revisa y compara la informacion de las diferentes monedas</p>
+        <div>
+          <form>
+            <div>
+              <select>
+                <option>Peso</option>
+                <option>Dollar</option>
+              </select>
+            </div>
+            <div>
+              <select>
+                <option>Dollar</option>
+                <option>Peso</option>
+              </select>
+            </div>
+            <div>
+              <button>Compare</button>
+            </div>
+          </form>
+        </div>
+      </article>
+      <article>
+        <p>Lee los blogs mas recientes sobre las monedas que te interesan</p>
+        <button>Echa un vistazo</button>
+      </article>
     </section>
   );
 }
