8c4b88982da39731d6bc7be571e008786bcc9193
Jesus Bossa <x@jesusbossa.dev>
2022-03-20 17:44:44 -0500

    chore: starting to add loading states

--- pages/article/[id].tsx
+++ pages/article/[id].tsx
@@ -1,9 +1,12 @@
 import { FluidContainer } from '@makinox/makinox-ui';
+import Head from 'next/head';
 import Image from 'next/image';
 import { useMemo } from 'react';
+import ContentLoader from 'react-content-loader';
 import useSWR from 'swr';
 
 import { fetcher } from '../../utils/fetcher';
+import isBrowser from '../../utils/isBrowser';
 import { NewComplete } from '../api/news';
 
 export async function getServerSideProps({ query }: { query: any }) {
@@ -32,19 +35,36 @@ export default function Article({ query }: { query: any }) {
   }, [data]);
 
   return (
-    <section>
-      <div>
-        <Image
-          height={350}
-          width={1500}
-          layout="responsive"
-          objectFit="cover"
-          src={bigImage || 'https://picsum.photos/id/1011/1500/350'}
-          alt={`Currencier: ${title}`}
-        />
-        <h1 className={`text-center ${FluidContainer()}`}>{title}</h1>
-      </div>
-      <article className={`${FluidContainer()}`}>{blogContent}</article>
-    </section>
+    <>
+      <Head>
+        <title>{title} - article: Currencier </title>
+      </Head>
+      <section>
+        <div>
+          <Image
+            height={350}
+            width={1500}
+            layout="responsive"
+            objectFit="cover"
+            src={bigImage || 'https://picsum.photos/id/1011/1500/350'}
+            alt={`Currencier: ${title}`}
+          />
+          {title ? (
+            <h1 className={`text-center ${FluidContainer()}`}>{title}</h1>
+          ) : (
+            <div className={FluidContainer()}>
+              <ContentLoader width={1200} height={100} viewBox={`0 0 ${1200} 40`} backgroundColor="#000">
+                <rect x="0" y="10" rx="10" ry="20" width={1200} height="20" />
+              </ContentLoader>
+            </div>
+          )}
+        </div>
+        {blogContent ? (
+          <article className={`${FluidContainer()}`}>{blogContent}</article>
+        ) : (
+          <article className={`${FluidContainer()}`}>blogContent</article>
+        )}
+      </section>
+    </>
   );
 }
